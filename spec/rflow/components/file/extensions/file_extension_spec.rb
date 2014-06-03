321c27a9fb1fafc5f77247f60afe135504ebbba8
John Stoneham <lyric@lyrically.net>
2014-06-03 15:44:07 -0400

    Namespace specs.

---
+++ spec/rflow/components/file/extensions/file_extension_spec.rb
@@ -0,0 +1,48 @@
+require 'spec_helper'
+require 'time'
+
+class RFlow
+  module Components
+    module File
+      module Extensions
+        describe FileExtension do
+          it "should add the extension to RFlow::Configuration" do
+            Configuration.available_data_extensions['RFlow::Message::Data::File'].should include(described_class)
+          end
+
+          it "should set the defaults" do
+            file = Message.new('RFlow::Message::Data::File').data.tap do |d|
+              d.path.should == '/'
+              d.size.should == 0
+              d.content.should == ''
+              d.creation_timestamp.should be_nil
+              d.modification_timestamp.should be_nil
+              d.access_timestamp.should be_nil
+            end
+          end
+
+          it "should correctly use integers or strings for size field" do
+            Message.new('RFlow::Message::Data::File').data.tap do |d|
+              d.size.should == 0
+              d.size = 10
+              d.size.should == 10
+              d.size = '20'
+              d.size == 20
+            end
+          end
+
+          it "should correctly use Time or xmlschema strings for timestamp fields" do
+            Message.new('RFlow::Message::Data::File').data.tap do |d|
+              now = Time.now
+              d.creation_timestamp.should == nil
+              d.creation_timestamp = now
+              d.creation_timestamp.should == Time.xmlschema(now.xmlschema(9))
+              d.creation_timestamp = now.xmlschema
+              d.creation_timestamp.should == Time.xmlschema(now.xmlschema)
+            end
+          end
+        end
+      end
+    end
+  end
+end
---
+++ spec/rflow/components/file/output_raw_to_files_spec.rb
@@ -0,0 +1,36 @@
+require 'spec_helper'
+
+class RFlow
+  module Components
+    module File
+      describe OutputRawToFiles do
+        let(:component_config) do
+          OpenStruct.new(:name         => 'port name',
+                         :uuid         => 0,
+                         :input_ports  => [],
+                         :output_ports => [],
+                         :options      => {
+                           'file_name_prefix' => 'boom',
+                           'file_name_suffix' => '.town',
+                           'directory_path'   => '/tmp'})
+        end
+
+        let(:component) { described_class.new(component_config) }
+
+        it "should correctly process file name prefix/suffix" do
+          component.send(:output_file_name).should match(/boom.*0001.town/)
+        end
+
+        it "should do stuff" do
+          message = Message.new('RFlow::Message::Data::Raw').tap do |m|
+            m.data.raw = 'boomertown'
+          end
+
+          output_file_path = component.process_message nil, nil, nil, message
+
+          ::File.exist?(output_file_path).should be true
+        end
+      end
+    end
+  end
+end
